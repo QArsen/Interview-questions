9488ba68e53414af346b64d47b0d11107af4beae
Arsen <audit1551@gmail.com>
2020-06-03 14:48:54 +0300

    Interview Questions, sort string

---
+++ Abecedarian.java
@@ -0,0 +1,26 @@
+package com.app.InterviewQuestions;
+
+public class Abecedarian {
+
+    public static void main(String[] args) {
+
+        System.out.println(isAbecedarian("abrcde"));
+    }
+
+
+
+    public static boolean isAbecedarian(String s) {
+        int index = s.length() - 1;
+
+        for (int i = 0; i < index; i++) {
+
+            if (s.charAt(i) <= s.charAt(i + 1)) {
+            } //Need to check if each letter for the whole word is less than the one before it
+
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
