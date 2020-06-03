9b3ed72c0cc1dcf886eb325ef7ea79b8256d4041
Arsen <audit1551@gmail.com>
2020-06-03 14:35:11 +0300

    Interview Questions, sort string

---
+++ SortString.java
@@ -0,0 +1,38 @@
+package com.app.InterviewQuestions;
+
+public class SortString {
+
+    static String[] str = {"Kamila", "Tatiana", "Arsen", "Evelina", "Alina"};
+
+    static String flag = "words";
+
+    public static void main(String[] args) {
+
+        for (String s : sortWordInString(str)) {
+            System.out.println(s);
+        }
+
+    }
+
+
+    static String[] sortWordInString(String[] str) {
+        String temp = "";
+        if (str.length == 0) {
+            return str;
+
+        } else {
+            for (int i = 0; i < str.length; i++) {
+                for (int j = i + 1; j < str.length; j++) {
+                    if (str[i].compareTo(str[j]) > 0) {
+                        temp = str[i];
+                        str[i] = str[j];
+                        str[j] = temp;
+                    }
+                }
+            }
+        }
+        return str;
+    }
+
+}
+
